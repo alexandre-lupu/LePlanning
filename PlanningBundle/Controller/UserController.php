e6b09c58bfbac61bbcbdf30ff39b7780f30b17cb
Alexandre Lupu <alexandre.lupu@etu.univ-orleans.fr>
2014-03-25 11:03:01 +0100

    Ajout d'utilisateur opérationnel

--- PlanningBundle/Controller/UserController.php
+++ PlanningBundle/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace Iut\PlanningBundle\Controller;
 
+use Iut\PlanningBundle\Entity\User;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -15,7 +17,20 @@ class UserController extends Controller
      
     public function addAction()
     {
-		return array(); 
+		$request = $this->getRequest();
+		$nom=$request->request->get('nom');
+		$mdp=$request->request->get('mdp');
+		
+		$user=new User();
+		$user->setName($nom);
+		$user->setPassword($mdp);
+		
+		$em = $this->getDoctrine()->getManager();
+		
+		$em->persist($user);
+		$em->flush();
+		
+		return $this->render('IutPlanningBundle:User:add.html.twig',array('name'=>$nom));
     }
 	
     /**
